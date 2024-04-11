1804ca372d8700fdd4141246c9a9e855e1f451c4
kimseunggyu <dev.seunggyu@gmail.com>
2024-04-11 19:56:37 +0900

    refactor: button 컴포넌트 위치 변경

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entryPoints: ["src/button.tsx"],
+  entry: ["src/components/index.ts"],
   format: ["cjs", "esm"],
   dts: true,
   sourcemap: true,
