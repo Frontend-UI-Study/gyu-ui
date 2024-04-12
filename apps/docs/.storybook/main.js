c5f275255e41b58ae4a23c64806d2dd19079262c
kimseunggyu <dev.seunggyu@gmail.com>
2024-04-12 21:36:33 +0900

    chore: 스토리북 addon 추가

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -1,18 +1,24 @@
-import { dirname, join, resolve } from "path";
+import { dirname, join } from 'path';
 
 function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
 
 const config = {
-  stories: ["../stories/*.stories.tsx", "../stories/**/*.stories.tsx"],
+  stories: ['../stories/*.stories.tsx', '../stories/**/*.stories.tsx'],
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-docs"),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-docs'),
+    // add addon
+    getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath('storybook-addon-pseudo-states'),
+    // figma 링크 추가하면 피그마 디자인 볼 수 있음. 근데 너무 느림..
+    // getAbsolutePath('@storybook/addon-designs'),
+    // https://storybook.js.org/addons/@storybook/test-runner/ <- 스토리북에서 테스트 코드 작성하고 싶다면
   ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
 
@@ -22,7 +28,7 @@ const config = {
     // customize the Vite config here
     return {
       ...config,
-      define: { "process.env": {} },
+      define: { 'process.env': {} },
     };
   },
 
