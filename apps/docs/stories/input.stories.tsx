4fd25ddc7925a3e763f96647479358a6e3a4dae7
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2024-04-19 22:32:43 +0900

    feature: Input (#3)
    * feat: input 기본 스타일 구현
    
    * feat: input variant 스타일 구현
    
    * feat: Input password 타입 구현
    
    * feat: Input ref 스토리북 작성
    
    * feat: Input storybook 변경

--- apps/docs/stories/input.stories.tsx
+++ apps/docs/stories/input.stories.tsx
@@ -1,14 +1,23 @@
+import { useState, useRef } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Input } from '@repo/ui/components';
 
 const meta: Meta<typeof Input> = {
-  title: 'test/Input',
+  title: 'DS/Input',
   component: Input,
-  argTypes: {
-    type: {
-      control: { type: 'radio' },
-      options: ['button', 'submit', 'reset'],
-    },
+  argTypes: {},
+  render: (props) => {
+    const [value, setValue] = useState('');
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value);
+    };
+
+    const onReset = () => {
+      setValue('');
+    };
+
+    return <Input {...props} onChange={onChange} onReset={onReset} value={value} />;
   },
 };
 
@@ -17,3 +26,62 @@ export default meta;
 type Story = StoryObj<typeof Input>;
 
 export const Default: Story = {};
+
+export const WithClear: Story = {
+  args: {
+    withClear: true,
+  },
+};
+
+export const WithSearch: Story = {
+  args: {
+    withSearch: true,
+  },
+};
+
+export const WithFullIcon: Story = {
+  args: {
+    withSearch: true,
+    withClear: true,
+  },
+};
+
+export const error: Story = {
+  args: {
+    error: true,
+  },
+};
+
+export const disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+//
+
+export const variants: Story = {
+  args: {
+    variant: 'underline',
+  },
+};
+
+export const password: Story = {
+  args: {
+    type: 'password',
+  },
+};
+
+export const withRef: Story = {
+  render: (props) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const onReset = () => {
+      if (inputRef.current) {
+        inputRef.current.value = '';
+      }
+    };
+
+    return <Input {...props} onReset={onReset} ref={inputRef} />;
+  },
+};
--- packages/ui/src/components/Input/index.tsx
+++ packages/ui/src/components/Input/index.tsx
@@ -1,22 +1,132 @@
 import * as React from 'react';
+import { useCallback, useState } from 'react';
+import { cva } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
+import { IconViewOff } from '@/components/Input/IconViewOff';
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+import { IconViewOn } from './IconViewOn';
+import { IconSearch } from './IconSearch';
+import { IconClose } from './IconClose';
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      className={cn(
-        'border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-        className,
-      )}
-      ref={ref}
-      type={type}
-      {...props}
-    />
-  );
+const inputWrapperVariants = cva(
+  'border-gray200 focus-within:border-gray700 placeholder:text-gray400 flex items-center justify-center rounded-md  px-4 py-2.5',
+  {
+    variants: {
+      error: {
+        true: 'focus-within:border-danger500 border-danger500',
+      },
+      disabled: {
+        true: 'border-gray200 bg-gray50',
+      },
+      variant: {
+        default: 'border',
+        underline: 'border-b rounded-none bg-transparent',
+      },
+    },
+  },
+);
+
+const inputVariants = cva('text-text700 w-full outline-none', {
+  variants: {
+    disabled: {
+      true: 'text-text300',
+    },
+    variant: {
+      default: '',
+      underline: '',
+    },
+  },
+  compoundVariants: [
+    {
+      disabled: true,
+      variant: 'underline',
+      class: 'bg-gray-white',
+    },
+  ],
 });
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  /**
+   * 에러 유무
+   */
+  error?: boolean;
+  /**
+   * 입력시 clear 할 수 있는 버튼 표시 (onReset 이벤트 필요)
+   */
+  withClear?: boolean;
+  /**
+   * Search Input 으로 SearchIcon 표시
+   */
+  withSearch?: boolean;
+  /**
+   * clear Icon 클릭시 이벤트
+   */
+  onReset?: () => void;
+  /**
+   * Input UI 유형
+   */
+  variant?: 'default' | 'underline';
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      variant = 'default', //
+      error,
+      withClear,
+      withSearch,
+      type,
+      className,
+      onReset,
+      ...props
+    },
+    ref,
+  ) => {
+    const [viewOn, setViewOn] = useState(false);
+
+    const handleReset = useCallback(() => {
+      onReset?.();
+    }, [onReset]);
+
+    const disabled = props.disabled;
+    const isPasswordType = type === 'password';
+    const _type = type === 'password' && viewOn ? 'text' : type;
+    return (
+      <div className={cn(inputWrapperVariants({ error, disabled, variant }))}>
+        <input
+          // TODO: group 써서 처리하기 - 현재 찾아보니깐, group 으로 하기 어려운거 같음.
+          // not 지원안함, 복자한 형제, 부모 제어를 위한 방식 없음?
+          className={cn(inputVariants({ disabled, variant }), 'input ', className)}
+          placeholder="placeholder"
+          ref={ref}
+          type={_type}
+          {...props}
+        />
+
+        <div className="flex items-center gap-3">
+          {withClear ? <IconClose className={cn('icon-close fill-gray-500')} onClick={handleReset} /> : null}
+          {isPasswordType ? (
+            viewOn ? (
+              <IconViewOn
+                onClick={() => {
+                  setViewOn(false);
+                }}
+              />
+            ) : (
+              <IconViewOff
+                onClick={() => {
+                  setViewOn(true);
+                }}
+              />
+            )
+          ) : null}
+          {withSearch ? <IconSearch className={cn('icon-search', disabled && 'fill-gray300')} /> : null}
+        </div>
+      </div>
+    );
+  },
+);
 Input.displayName = 'Input';
 
 export { Input };
